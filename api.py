56036fb7dd4fb20327ce5f0155313c6d069be97f
nelliejellie <emekaewelike@gmail.com>
2024-10-22 11:47:36 +0100

    new changes

--- api.py
+++ api.py
@@ -1,12 +1,33 @@
 from fastapi import FastAPI
 from data import projectsOut
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True, 
+    allow_methods=["*"], 
+    allow_headers=["*"],
+)
 @app.get("/")
 def index():
-    return {"name":"Welcome to Arctech"}
+    return {"name":"Welcome to Artech"}
 
 @app.get("/projects")
 def get_projects():
     return projectsOut 
+
+@app.get("/get-project/{project_id}")
+def get_project(project_id:int):
+    for project in projectsOut:
+        if project["id"] == project_id:
+            print(project)
+            return project
+    return {"message":"data does not exist"}
--- data.py
+++ data.py
@@ -1,23 +1,30 @@
 projectsOut = [
     {
         "id": 1,
-        "title": "Project One",
-        "description": "This is the first project.",
-        "image_url": "https://example.com/image1.jpg",
-        "project_url": "https://example.com/project1"
+        "title": "Home Automation",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam nemo eligendi fuga quis maiores odit ipsum placeat eveniet, nulla nobis maxime impedit aliquid repellendus. Expedita iste id assumenda doloremque architecto.",
+        "image_url": "https://media.istockphoto.com/id/2039918088/photo/happy-african-american-woman-using-tablet-on-a-cozy-sofa-at-home.webp?a=1&b=1&s=612x612&w=0&k=20&c=a124M439yRMrnReLVn91U6BgfIDGVMiJDZCTpBAgib8=",
+        "project_url": "http://localhost:8000/get-project/1"
     },
     {
         "id": 2,
-        "title": "Project Two",
-        "description": "This is the second project.",
-        "image_url": "https://example.com/image2.jpg",
-        "project_url": "https://example.com/project2"
+        "title": "City AI",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam nemo eligendi fuga quis maiores odit ipsum placeat eveniet, nulla nobis maxime impedit aliquid repellendus. Expedita iste id assumenda doloremque architecto.",
+        "image_url": "https://media.istockphoto.com/id/2155769555/photo/artificial-intelligence-concept-cpu-quantum-computing.webp?a=1&b=1&s=612x612&w=0&k=20&c=n1kRrOwYLlUSELAMpbKZXgrL-a9XFJ_h-q0F2xdCTig=",
+        "project_url": "http://localhost:8000/get-project/2"
     },
     {
         "id": 3,
-        "title": "Project Three",
-        "description": "This is the third project.",
-        "image_url": "https://example.com/image3.jpg",
-        "project_url": "https://example.com/project3"
+        "title": "Ocean Waves",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam nemo eligendi fuga quis maiores odit ipsum placeat eveniet, nulla nobis maxime impedit aliquid repellendus. Expedita iste id assumenda doloremque architecto.",
+        "image_url": "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDg0fHx3YXZlc3xlbnwwfHx8fDE2NjE1Njg5MTg&ixlib=rb-4.0.3&q=80&w=1080",
+        "project_url": "http://localhost:8000/get-project/3"
+    },
+    {
+        "id": 4,
+        "title": "AI Security",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam nemo eligendi fuga quis maiores odit ipsum placeat eveniet, nulla nobis maxime impedit aliquid repellendus. Expedita iste id assumenda doloremque architecto.",
+        "image_url": "https://media.istockphoto.com/id/1777088857/photo/digital-security-concept.webp?a=1&b=1&s=612x612&w=0&k=20&c=ZZS0p9fbcn-KXSX19EYbntNRFcwvXtU2fTWZUEwQIyc=",
+        "project_url": "http://localhost:8000/get-project/4"
     }
 ]
